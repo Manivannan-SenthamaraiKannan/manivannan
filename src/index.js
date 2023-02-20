4f11a8f59c4800b90844ee9ab7a692a7acd284fe
Manivannan S <sharvamanivannan@gmail.com>
2023-02-20 10:16:37 +0000

    initial setup and NAVBAR

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import * as THREE from "three";
+// import NET from 'vanta/dist/vanta.net.min';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+// const section = document.getElementsByTagName('section')[0]
+// NET({
+//   el: section,
+//   THREE,
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   scale: 1.00,
+//   scaleMobile: 1.00
+// })
+
+
